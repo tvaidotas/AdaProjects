6a991ccd388a738ce25b978575984fac6929476e
tvaidotas <tadas.vaidotas@gmail.com>
2020-05-08 16:19:57 +0100

    added a hello world example

---
+++ HelloWorld/src/main.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO; -- import
+use Ada.Text_IO; -- static import
+
+procedure Main is -- procedure is void
+begin
+   Put_Line ("Hello, World! 2019"); -- this is a comment going to the end of line
+end Main;
