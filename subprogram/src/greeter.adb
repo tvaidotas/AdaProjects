7e06619ccd9cfad860aa77ed7a4ed7d3e3846d99
tvaidotas <tadas.vaidotas@gmail.com>
2020-05-11 13:51:19 +0100

    updating subprogram to cut the empty characters

--- subprogram/src/greeter.adb
+++ subprogram/src/greeter.adb
@@ -3,14 +3,14 @@ package body Greeter is
 
    -- the actual implementation goes into here
    
-   function Read_Name return String is
-      S : String (1 .. 10) := (others => ' ');
-      Last: Integer;
+   function Read_Name return String  is
+      S    : String (1 .. 10);
+      Last : Integer;
    begin
-	 Put ("Enter your name: ");
-	 Get_Line(S, Last);
-	 Put_Line ("");
-	 return "Hello " & S & ", it's nice to meet you.";
+      Put ("Enter your name: ");
+      Get_Line (S, Last);
+      Put_Line ("");
+      return "Hello " & S (1 .. Last) & ", it's nice to meet you.";
    end Read_Name;
 
 end Greeter;
