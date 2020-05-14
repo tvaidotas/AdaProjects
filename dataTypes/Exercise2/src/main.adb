ce7b855bb27a0820e00e5db347ac0e5899be62c5
tvaidotas <tadas.vaidotas@gmail.com>
2020-05-14 10:37:36 +0100

    added additional exercises for the data type

---
+++ dataTypes/Exercise2/src/main.adb
@@ -0,0 +1,18 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+
+   type Bottles is range 0 .. 99;
+   A : Bottles := 500;
+begin
+   for I in reverse Bottles loop
+      if I > 0 then
+	 Put_Line (Bottles'Image (I) & " bottles of beer on the wall, " & Bottles'Image (I) & " bottles of beer.");
+	 Put_Line ("Take one down and pass it around, " & Bottles'Image (I) & "  bottles of beer on the wall.");
+      else
+	 Put_Line ("No more bottles of beer on the wall, no more bottles of beer.");
+	 Put_Line ("Go to the store and buy some more, 99 bottles of beer on the wall.");
+      end if;
+      Put_Line ("");
+   end loop;
+end Main;
